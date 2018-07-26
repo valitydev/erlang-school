05bc91612a5fe5f2e5c3657f56d52165fc54e950
Kehitt <devstealth@gmail.com>
2018-07-26 13:01:24 +0300

    fix syntax in tasks 1 and 2

--- task_2_1.erl
+++ task_2_1.erl
@@ -3,7 +3,7 @@
 %% API
 -export([test/0]).
 
--define(safe_handle(Func), try Func() catch Error:Reason -> {error, Error, Reason} end).
+-define(safe_handle(Func), try Func catch Error:Reason -> {error, Error, Reason} end).
 
 test() ->
-  ?safe_handle(fun() -> 1 = one end).
+  ?safe_handle(1 = one).
--- task_2_2.erl
+++ task_2_2.erl
@@ -3,8 +3,8 @@
 %% API
 -export([test/0]).
 
--define(verbose_handle(Func), try Func() catch Error:Reason -> io:format("Error! ~p ~p~n", [Error, Reason]), throw({Error, Reason}) end).
+-define(verbose_handle(Func), try Func catch Error:Reason -> io:format("Error! ~p ~p~n", [Error, Reason]), erlang:raise(Error, Reason, erlang:get_stacktrace()) end).
 
 test() ->
-  ?verbose_handle(fun() -> 1 = one end).
+  ?verbose_handle(1 = one).
 
